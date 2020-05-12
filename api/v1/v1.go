bea0ee7f824fb08516398290e33e07245e68f962
Flynn <flynn@breaktime.com.tw>
2020-05-12 17:43:19 +0800

    Add new api called open_id

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -8,6 +8,7 @@ func ApplyRoutes(r *gin.Engine) {
 		v1.POST("/footprint", footprint)
 		v1.POST("/profile_oath", profileOath)
 		v1.POST("/wifi", wifi)
+		v1.POST("/open_id", openId)
 		v1.GET("/cat_trid", catTrid)
 	}
 }
